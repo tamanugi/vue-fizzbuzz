2ba7a721c68ac6324cb8a3edcd9151cf4820d4f5
tamanugi <tamanugi@gmail.com>
2017-01-03 22:50:12 +0900

    implements component

---
+++ src/components/Buttons.vue
@@ -0,0 +1,18 @@
+
+<template>
+  <div class="buttons">
+    <button @click="increment" >+</button>
+    <button @click="decrement" >-</button>
+  </div>
+</template>
+
+<script>
+import {mapActions} from 'vuex'
+
+export default {
+  methods: mapActions([
+    'increment',
+    'decrement'
+  ])
+}
+</script>
---
+++ src/components/FizzBuzz.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <div v-if="fizzbuzz">{{ fizzbuzz }}</div>
+    <div v-else>{{ $store.state.count }}</div>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+  computed: mapGetters([
+    'fizzbuzz'
+  ])
+}
+</script>
